08ace602bbf523a0505d51ed5d385d2b00c21d1e
Leonora <leonorap@kth.se>
2018-10-05 09:52:55 +0200

    change site metadata

--- src/siteMeta.js
+++ src/siteMeta.js
@@ -1,4 +1,4 @@
 const SITE_HEADLINE = 'Welcome to React Photo Gallery';
-const GITHUB_URL = 'https://github.com/leonorpan/spo-gallery';
+const GITHUB_URL = 'https://github.com/leonorpan/spo-photo-gallery';
 
 export { SITE_HEADLINE, GITHUB_URL };
